41f1cbf587a1c23778234f8c3e45e167c3dd7419
AN-HA\AN HA <baoandlk93@gmail.com>
2024-10-01 14:42:38 +0700

    init project

--- src/main/java/com/codgym/user_management/exception/ExceptionHandling.java
+++ src/main/java/com/codgym/user_management/exception/ExceptionHandling.java
@@ -2,9 +2,15 @@ package com.codgym.user_management.exception;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 @ComponentScan("com.codgym.user_management")
 public class ExceptionHandling {
 
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception exception) {
+        return new ModelAndView("error", "message", exception.getMessage());
+    }
 }
