47c97f7a51b2a456e81aefb190c10482f39009a5
AN-HA\AN HA <baoandlk93@gmail.com>
2024-10-04 14:47:28 +0700

    done update data

--- src/main/java/com/codgym/user_management/controller/CustomerRestController.java
+++ src/main/java/com/codgym/user_management/controller/CustomerRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,16 @@ public class CustomerRestController {
     }
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Customer customer) {
+        customerService.save(customer);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> findById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(customerService.findById(id), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody Customer customer) {
         customerService.save(customer);
         return new ResponseEntity<>(HttpStatus.OK);
     }
